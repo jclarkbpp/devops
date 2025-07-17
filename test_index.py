bf6f229e7590b3a381fa5814c979880cb93aa8a4
Joshua Clark <josh.clark@kyndryl.com>
2025-07-17 15:07:38 +0100

    second commit

--- test_index.py
+++ test_index.py
@@ -1,10 +1,15 @@
 import pytest
  
-from index import add
+from index import add, minus
  
 def test_add():
     assert add(1, 2) == 3
     assert add(14, 15) == 29
     with pytest.raises(TypeError):
         add(4, 'Three-Thousand')
+
+def test_minus():
+    assert minus(5, 3) == 2
+    assert minus(6, 9) == -3
+    assert minus(11, 4) != 4
  
